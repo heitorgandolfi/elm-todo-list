371f7d3ec6ba7e5c7fcd264367c5f2825210055e
heitorgandolfi <heitor.gandolfi@hotmail.com>
2024-06-29 11:10:57 -0300

    feat: add update task function

--- src/Main.elm
+++ src/Main.elm
@@ -1,8 +1,11 @@
 module Main exposing (..)
 
 import Browser
-import Html exposing (Html, button, div, h1, img, input, p, section, span, text)
-import Html.Attributes exposing (class, classList, id, name, placeholder, src, type_, value)
+import FontAwesome as Icon exposing (Icon)
+import FontAwesome.Attributes as Icon
+import FontAwesome.Solid as Icon
+import Html exposing (Html, button, div, h1, i, img, input, p, section, span, text)
+import Html.Attributes exposing (alt, class, classList, id, name, placeholder, src, type_, value)
 import Html.Events exposing (onClick, onInput)
 
 
@@ -17,10 +20,11 @@ main =
 
 
 -- MODEL
+-- Change the id type to String to use the UUID library
 
 
 type alias Task =
-    { id : Int, description : String }
+    { id : Int, description : String, isDone : Bool }
 
 
 type alias Error =
@@ -39,12 +43,12 @@ deleteIconPath =
 init : Model
 init =
     { tasks =
-        [ Task 1 "Task 1"
-        , Task 2 "Task 2"
-        , Task 3 "Task 3"
+        [ Task 1 "Task 1" True
+        , Task 2 "Task 2" False
+        , Task 3 "Task 3" True
         ]
     , newTaskDescription = ""
-    , errorState = { state = False, message = "" }
+    , errorState = Error False ""
     }
 
 
@@ -56,6 +60,7 @@ type Msg
     = CreateTask
     | UpdateNewTaskDescription String
     | DeleteTask Int
+    | UpdateTask Int
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -64,16 +69,16 @@ update msg model =
         CreateTask ->
             case model.newTaskDescription of
                 "" ->
-                    ( { model | errorState = { state = True, message = "Test" } }, Cmd.none )
+                    ( { model | errorState = Error True "It is not possible to insert a new blank task." }, Cmd.none )
 
                 _ ->
                     let
+                        -- This is a temporary solution to generate unique IDs. I will change it later when i implemented the delete function.
                         taskId =
                             List.length model.tasks + 1
 
-                        -- This is a temporary solution to generate unique IDs. I will change it later when i implemented the delete function.
                         newTask =
-                            Task taskId model.newTaskDescription
+                            Task taskId model.newTaskDescription False
                     in
                     ( { model
                         | tasks = newTask :: model.tasks
@@ -87,7 +92,31 @@ update msg model =
             ( { model | newTaskDescription = newTask }, Cmd.none )
 
         DeleteTask taskId ->
-            ( { model | tasks = List.filter (\task -> task.id /= taskId) model.tasks }, Cmd.none )
+            ( { model | tasks = filterTaskToDelete taskId model.tasks }, Cmd.none )
+
+        UpdateTask taskId ->
+            let
+                completedTask =
+                    updateTask taskId model.tasks
+            in
+            ( { model | tasks = completedTask }, Cmd.none )
+
+
+filterTaskToDelete : Int -> List Task -> List Task
+filterTaskToDelete taskId =
+    List.filter (\task -> task.id /= taskId)
+
+
+updateTask : Int -> List Task -> List Task
+updateTask taskId =
+    List.map
+        (\task ->
+            if task.id == taskId then
+                { task | isDone = not task.isDone }
+
+            else
+                task
+        )
 
 
 
@@ -113,6 +142,7 @@ view model =
                         []
                     , button [ onClick CreateTask ] [ text "+" ]
                     ]
+                , renderErrorMessage model.errorState.message
                 ]
             , div [ id "divide" ] []
             , renderTasks <| model.tasks
@@ -131,12 +161,27 @@ renderTasks tasks =
         section [] (List.map taskCardView <| tasks)
 
 
+renderErrorMessage : String -> Html Msg
+renderErrorMessage message =
+    if message == "" then
+        text ""
+
+    else
+        p [ class "error-message" ] [ text message ]
+
+
 taskCardView : Task -> Html Msg
 taskCardView task =
     div [ class "task-card" ]
         [ div []
-            [ span [] [ text "ícone" ]
-            , p [] [ text task.description ]
+            [ i [ onClick (UpdateTask task.id), classList [ ( "task-done", task.isDone ) ] ]
+                [ if task.isDone then
+                    Icon.view Icon.checkCircle
+
+                  else
+                    Icon.view Icon.circle
+                ]
+            , p [ classList [ ( "task-done", task.isDone ) ] ] [ text task.description ]
             ]
-        , img [ onClick <| DeleteTask task.id, src deleteIconPath ] [ text "Excluir" ]
+        , i [ onClick <| DeleteTask task.id, alt "delete task icon", class "remove-task-icon" ] [ Icon.view Icon.trashCan ]
         ]
